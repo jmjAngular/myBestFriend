484131a291b5c3e4dc4c7f0bc80499325b14c80f
Joseph M J <joseph.mj@cordiant.com>
2016-07-15 19:08:27 +0530

    My best friend details

---
+++ app/app.component.ts
@@ -0,0 +1,34 @@
+import {Component} from '@angular/core';
+
+@Component({
+    selector: 'my-app',
+    template: `
+              <h1>{{title}}</h1>
+              <h2>{{best_friend.name}} details!</h2>
+              <div><label> Mobile: </label>{{best_friend.mobile}}</div>
+              <div>
+                <label>Name: </label>
+                <input  [(ngModel)]="best_friend.name" placeholder="name">
+            
+              </div>
+              `,
+})
+export class AppComponent {
+    title = "My Best Friend";
+    best_friend:MyBestFriend = {
+        mobile: 495367667,
+        name: 'Jomon John'
+    };
+}
+
+export class MyBestFriend {
+    mobile:number;
+    name:string;
+
+}
+
+/*
+ Copyright 2016 Google Inc. All Rights Reserved.
+ Use of this source code is governed by an MIT-style license that
+ can be found in the LICENSE file at http://angular.io/license
+ */
---
+++ readme.md
@@ -0,0 +1,11 @@
+## My Best Friend
+### Simple Angular2 model binding example
+
+
+`git clone  https://github.com/jmjangular2/myBestFriend.git`
+
+
+
+###### Just used CDN for Angular2 libraries.
+####### Explore Angular2
+_JMJ_
